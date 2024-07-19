55196fc918160cbb661c0525816405b9a44cbe3d
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-07-19 19:33:17 +0300

    hw11

---
+++ src/js/render-functions.js
@@ -0,0 +1,38 @@
+export function renderImages(arr) {
+  return arr.hits
+    .map(image => {
+      return `<li class="img-item">
+  <a href="${image.largeImageURL}" class="img-link">
+    <img src="${image.webformatURL}" alt="${image.tags}" class="img-picture" />
+  </a>
+  <ul class="img-stats">
+    <li>
+      <p class="img-text">
+        Likes
+        <span class="img-stat">${image.likes}</span>
+      </p>
+    </li>
+    <li>
+      <p class="img-text">
+        Views
+        <span class="img-stat">${image.views}</span>
+      </p>
+    </li>
+    <li>
+      <p class="img-text">
+        Comments
+        <span class="img-stat">${image.comments}</span>
+      </p>
+    </li>
+    <li>
+      <p class="img-text">
+        Downloads
+        <span class="img-stat">${image.downloads}</span>
+      </p>
+    </li>
+  </ul>
+</li>
+`;
+    })
+    .join('');
+}
--- src/main.js
+++ src/main.js
@@ -5,8 +5,70 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { fetchImages } from './js/pixabay-api';
+import { renderImages } from './js/render-functions';
 
 const refs = {
-  btn: document.querySelector('.btn'),
   form: document.querySelector('.form'),
+  list: document.querySelector('.img-list'),
+  imgList: document.querySelector('.img-item'),
+  loader: document.querySelector('.loader'),
 };
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const query = e.target.elements.searchInput.value.trim();
+  if (query === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please type your request!',
+      position: 'topRight',
+      backgroundColor: '#EF4040',
+      iconColor: '#fff',
+      messageColor: '#fff',
+      titleColor: '#fff',
+    });
+    return;
+  }
+  e.target.reset();
+  refs.loader.classList.remove('visually-hidden');
+
+  const res = fetchImages(query)
+    .then(qRes => {
+      if (qRes.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+          backgroundColor: '#EF4040',
+          iconColor: '#fff',
+          messageColor: '#fff',
+          titleColor: '#fff',
+        });
+        return;
+      }
+
+      return renderImages(qRes);
+    })
+    .then(markup => {
+      refs.list.innerHTML = markup;
+      refs.loader.classList.add('visually-hidden');
+      new SimpleLightbox('.img-list a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: `Sorry, something went wrong: ${error.message}`,
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        iconColor: '#fff',
+        messageColor: '#fff',
+        titleColor: '#fff',
+      });
+    });
+}
